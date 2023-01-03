1e453e182f26f0feaba8b0b809abc870172df47d
StarsEnd <thestarsend@qq.com>
2023-01-03 16:58:58 +0800

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'saos.ts'),
+      name: 'saos',
+      // the proper extensions will be added
+      fileName: 'saos'
+    },
+    rollupOptions: {
+      // 确保外部化处理那些你不想打包进库的依赖
+      external: ['vue','vite-plugin-dts','vite'],
+      output: {
+        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  },
+  plugins: []
+})
